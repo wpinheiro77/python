b02e54638b3cc3280db22f844249e8b08423b2c1
wpinheiro <william.pinheiro@fatec.sp.gov.br>
2018-06-21 17:19:48 -0300

    Strings

---
+++ Guanabara/Aula strings01.py
@@ -0,0 +1,4 @@
+nome= str(input('Digite o seu nome: '))
+print(nome.upper())
+print(nome.lower())
+print(len(nome.strip()))
---
+++ Guanabara/ex022.py
@@ -0,0 +1,4 @@
+nome = str(input('Digite o seu nome: ')).strip()
+print(nome.upper())
+print(nome.lower())
+print(len(nome) - nome.count(' '))
